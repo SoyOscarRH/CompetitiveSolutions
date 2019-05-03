3a4d30bad0963f0215b076aaf9d356d416cc32b4
Oscar Rosas <soyoscarrh@gmail.com>
2019-05-02 19:09:32 -0500

    Add new problem

---
+++ CodeForces/MaximumTree.cpp
@@ -0,0 +1,32 @@
+//@see https://vjudge.net/problem/Gym-101466B
+
+#include <vector>
+#include <iostream>
+#include <cstdint>
+
+
+int main() {
+    using namespace std;
+
+    int64_t numChildren, x;
+    cin >> numChildren;
+    vector<int64_t> children (numChildren);
+
+    for (int i = 0; i < numChildren; i++) {
+        cin >> x;
+        children[i] = x * -1;
+    }
+
+    sort(children.begin(), children.end());
+
+    int64_t result {}, lastLevel {1}, thisLevel;
+    for (auto child : children) {
+        thisLevel = lastLevel * (child * -1);
+        result += thisLevel;
+        lastLevel = thisLevel;
+    }
+
+    cout << result + 1 << endl;
+
+    return 0;
+}
