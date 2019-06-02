2fee45ff6ec1c58734d4c1b30a4ccd1a841fa1bc
Oscar Rosas <soyoscarrh@gmail.com>
2019-06-02 02:05:42 -0500

    At leat I made something

--- Leetcode/167-TwoSumII-InputArrayIsSorted.cpp
+++ Leetcode/167-TwoSumII-InputArrayIsSorted.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-auto twoSum(const vector<int>& numbers, int target) -> vector<int> {
-    int i, j {};
-    for (auto it {numbers.begin()}, end {numbers.end()}; it != end; ++it) {
-        const auto toFind = target - *it;
-        const auto result = lower_bound(it + 1, end, toFind);
-    
-        if (result != end and *result == toFind) {
-            const int i {it - numbers.begin()};
-            const int j {result - numbers.begin()};
-            return {i + 1, j + 1};
-        }
+auto twoSum(const vector<int> &numbers, int target) -> vector<int> {
+  int i, j {};
+  for (auto it{numbers.begin()}, end{numbers.end()}; it != end; ++it) {
+    const auto toFind = target - *it;
+    const auto result = lower_bound(it + 1, end, toFind);
+
+    if (result != end and *result == toFind) {
+      const int i {it - numbers.begin()};
+      const int j {result - numbers.begin()};
+      return {i + 1, j + 1};
     }
-    
-    return {0, 0};
+  }
+
+  return {0, 0};
 }
