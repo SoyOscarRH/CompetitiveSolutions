bdf4f53499e09c6088e915cede38f9029826b723
SoyOscarRH <soyoscarrh@gmail.com>
2019-07-23 22:25:48 -0500

    Fix and add better code to 980

--- Leetcode/980-UniquePathsIII.cpp
+++ Leetcode/980-UniquePathsIII.cpp
@@ -5,56 +5,48 @@ using namespace std;
 class Solution {
   template <typename T>
   using matrix = std::vector<std::vector<T>>;
-  enum class squareType : int { start = 1, end = 2, empty = 0, obstacle = -1 };
+  enum class square : int { start = 1, end = 2, empty = 0, obstacle = -1 };
 
  private:
-  const vector<int> moves {1, -1};
-  matrix<bool> visited;
   size_t xAxis, yAxis;
   int numberOfWays;
 
-
  public:
-  auto findWay(const matrix<squareType>& grid, int x, int y, int squaresToVisit) -> void {
-    const auto currentSquare = grid[x][y];
-    const auto toVisit = squaresToVisit -1;
+  auto findWay(matrix<square>& grid, int x, int y, int squaresToVisit) -> void {
+    if (x < 0 or x >= xAxis or y < 0 or y >= yAxis) return;
 
-    if (currentSquare == squareType::obstacle) return;
-    if (currentSquare == squareType::end) {
+    const auto currentSquare = grid[x][y];
+
+    if (currentSquare == square::obstacle) return;
+    if (currentSquare == square::end) {
       if (squaresToVisit == 0) ++numberOfWays;
       return;
     }
 
-    visited[x][y] = true;
+    grid[x][y] = square::obstacle;
 
-    for (auto move : moves) {
-      const auto newX = x + move;
-      if (0 <= newX and newX < xAxis and not visited[newX][y]) 
-          findWay(grid, newX, y, toVisit);
-    }
+    const auto toVisit = squaresToVisit - 1;
 
-    for (auto move : moves) {
-      const auto newY = y + move;
-      if (0 <= newY and newY < yAxis and not visited[x][newY]) 
-          findWay(grid, x, newY, toVisit);
-    }
+    findWay(grid, x - 1, y, toVisit);
+    findWay(grid, x + 1, y, toVisit);
+    findWay(grid, x, y - 1, toVisit);
+    findWay(grid, x, y + 1, toVisit);
 
-    visited[x][y] = false;
+    grid[x][y] = currentSquare;
   }
 
   auto uniquePathsIII(matrix<int>& originalGrid) -> int {
-    const auto& grid = reinterpret_cast<matrix<squareType>&>(originalGrid);
+    auto& grid = reinterpret_cast<matrix<square>&>(originalGrid);
 
     xAxis = grid.size(), yAxis = grid[0].size();
-    visited = matrix<bool>(xAxis, vector<bool>(yAxis, false));
 
-    auto squaresToVisit = 1;    // We have to count the start
+    auto squaresToVisit = 1;  // We have to count the start
     auto startingX = 0, startingY = 0;
     for (auto x = 0; x < xAxis; ++x) {
       for (auto y = 0; y < yAxis; ++y) {
         auto element = grid[x][y];
-        if (element == squareType::empty) ++squaresToVisit;
-        if (element == squareType::start) startingX = x, startingY = y;
+        if (element == square::empty) ++squaresToVisit;
+        if (element == square::start) startingX = x, startingY = y;
       }
     }
 
