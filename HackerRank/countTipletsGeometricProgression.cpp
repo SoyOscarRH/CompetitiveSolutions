6ae0e97078f0dab6dc27c55feb09dffb1dc61823
SoyOscarRH <soyoscarrh@gmail.com>
2019-09-06 00:33:58 -0500

    Update

--- HackerRank/countTipletsGeometricProgression.cpp
+++ HackerRank/countTipletsGeometricProgression.cpp
@@ -1,12 +1,12 @@
 long countTipletsGeometricProgression(vector<long>& data, long ratio) {
-    long counter {};
-    map<long, long> element2, element3;
+  long counter {};
+  map<long, long> element2, element3;
 
-    for (auto now: data) {
-        element2[now * ratio]++;
-        if (element2.count(now)) element3[now * ratio] += element2[now];
-        if (element3.count(now)) counter += element3[now];
-    }
+  for (auto now : data) {
+    element2[now * ratio]++;
+    if (element2.count(now)) element3[now * ratio] += element2[now];
+    if (element3.count(now)) counter += element3[now];
+  }
 
-    return counter;
-} 
+  return counter;
+}
