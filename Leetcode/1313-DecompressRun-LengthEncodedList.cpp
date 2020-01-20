09326c6fab1e942752382c33cc308397094241ab
SoyOscarRH <soyoscarrh@gmail.com>
2020-01-20 13:50:54 -0600

    Update 1313-DecompressRun-LengthEncodedList.cpp

--- Leetcode/1313-DecompressRun-LengthEncodedList.cpp
+++ Leetcode/1313-DecompressRun-LengthEncodedList.cpp
@@ -4,7 +4,7 @@ class Solution {
     auto result = vector<int> {};
     for (auto i = 0; i < nums.size(); i += 2) {
       auto const frequency = nums[i], value = nums[i + 1];
-      for (auto i = 0; i < frequency; ++i) result.push_back(value);
+      result.insert(end(result), frequency, value);
     }
 
     return result;
