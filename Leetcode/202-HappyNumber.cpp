228d423b8920a2a99057c6559cff945404ffdda9
SoyOscarRH <soyoscarrh@gmail.com>
2020-04-02 11:54:37 -0600

    Update 202-HappyNumber.cpp

--- Leetcode/202-HappyNumber.cpp
+++ Leetcode/202-HappyNumber.cpp
@@ -1,19 +1,23 @@
 class Solution {
- public:
-  auto isHappy(int n) -> bool {
-    auto seen = unordered_set<int> {1};
+public:
+  auto isHappy(const int n) -> bool {
+    auto visited = unordered_set<int>{};
+    auto current = n;
 
-    while (not seen.count(n)) {
-      seen.insert(n);
-
-      auto future = 0;
-      for (auto digit = 0; n != 0; n /= 10) {
-        digit = n % 10;
-        future += digit * digit;
+    while (true) {
+      auto next = 0;
+      for (auto digit = 0; current != 0; current /= 10) {
+        digit = current % 10;
+        next += digit * digit;
       }
-      n = future;
+
+      if (next == 1) return true;
+      if (visited.count(next)) return false;
+
+      visited.insert(next);
+      current = next;
     }
 
-    return n == 1;
+    return false;
   }
 };
