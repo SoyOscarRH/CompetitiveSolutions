b80f506aadfafd4c578fdc0536404d5076e3b319
SoyOscarRH <soyoscarrh@gmail.com>
2020-04-05 13:51:21 -0500

    Update 283-MoveZeroes.cpp

--- Leetcode/283-MoveZeroes.cpp
+++ Leetcode/283-MoveZeroes.cpp
@@ -3,8 +3,6 @@ class Solution {
   auto moveZeroes(vector<int>& nums) -> void {
     auto current = begin(nums);
     for (const auto num : nums) if (num) *(current++) = num;
-    transform(current, end(nums), current, [](auto _) { return 0; });
+    fill(current, end(nums), 0);
   }
 };
-
-
