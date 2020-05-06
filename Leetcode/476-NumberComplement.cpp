2c0bd064df8e175de27649b68492b9c136ca78f9
SoyOscarRH <soyoscarrh@gmail.com>
2020-05-06 02:48:37 -0500

    Update 476-NumberComplement.cpp

--- Leetcode/476-NumberComplement.cpp
+++ Leetcode/476-NumberComplement.cpp
@@ -1,13 +1,13 @@
 class Solution {
  public:
-  int findComplement(const int num) {
-    auto sum = 1, pow = 1;
-
-    while (sum < num) {
-      pow *= 2;
-      sum += pow;
+  auto findComplement(int num) -> int {
+    auto result = bitset<32> {};
+    for (auto i = 0; num; ++i) {
+      const auto bit_on = num bitand 1;
+      result[i] = bit_on ? 0 : 1;
+      num = num >> 1;
     }
 
-    return sum - num;
+    return result.to_ulong();
   }
 };
