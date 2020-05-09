7c0d5fa9ba6bc475f8b81840251e117cb6a0fda6
SoyOscarRH <soyoscarrh@gmail.com>
2020-05-09 06:19:10 -0500

    Create 367-ValidPerfectSquare.cpp

---
+++ Leetcode/367-ValidPerfectSquare.cpp
@@ -0,0 +1,16 @@
+struct Solution {
+  auto isPerfectSquare(const int num) -> bool {
+    auto left = 1, right = num;
+
+    while (left <= right) {
+      auto mid = left + (right - left) / 2;
+      if (mid * mid == num) return true;
+
+      if (mid < num / mid) left = mid + 1;
+      else  right = mid - 1;
+    }
+    return false;
+
+    return false;
+  }
+}
