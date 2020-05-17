0d648e67cab3b2f31eb767e69a493e7440d8f50e
SoyOscarRH <soyoscarrh@gmail.com>
2020-05-17 09:24:24 -0500

    Update 438-FindAllAnagramsInAString.cpp

--- Leetcode/438-FindAllAnagramsInAString.cpp
+++ Leetcode/438-FindAllAnagramsInAString.cpp
@@ -1,24 +1,27 @@
 struct Solution {
-  auto all_zeroes(const array<int, 26>& info) -> bool {
-    return all_of(cbegin(info), cend(info), [](const auto x) { return x == 0; });
+  auto all_zeroes(const array<int, 26>& letters) -> bool {
+    const auto zero = [](const auto x) { return x == 0; };
+    return all_of(cbegin(letters), cend(letters), zero);
   }
+
   auto findAnagrams(const string& s, const string& p) -> vector<int> {
     const auto n = s.size(), m = p.size();
     if (n < m) return {};
 
-    array<int, 26> info {};
-    for (const auto letter : p) ++info[letter - 'a'];
-    for (int i = 0; i < m; ++i) --info[s[i] - 'a'];
+    array<int, 26> letters {};
+    for (const auto letter : p) ++letters[letter - 'a'];
+    for (int i = 0; i < m; ++i) --letters[s[i] - 'a'];
 
-    auto result = all_zeroes(info) ? vector<int> {0} : vector<int> {};
+    auto start_index = vector<int> {};
+    if (all_zeroes(letters)) start_index.push_back(0);
 
     // substring [left, right)
     for (int right = m, left = 0; right < n; ++right, ++left) {
-      --info[s[right] - 'a'];
-      ++info[s[left] - 'a'];
-      if (all_zeroes(info)) result.push_back(left + 1);
+      --letters[s[right] - 'a'];
+      ++letters[s[left] - 'a'];
+      if (all_zeroes(letters)) start_index.push_back(left + 1);
     }
 
-    return result;
+    return start_index;
   }
 };
