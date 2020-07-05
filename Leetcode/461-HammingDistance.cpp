7b53bfff7300f986f0ab91aa66414c7407c935b8
SoyOscarRH <soyoscarrh@gmail.com>
2020-07-05 13:51:29 -0500

    Update 461-HammingDistance.cpp

--- Leetcode/461-HammingDistance.cpp
+++ Leetcode/461-HammingDistance.cpp
@@ -1,4 +1,16 @@
-class Solution {
- public:
-  int hammingDistance(int x, int y) { return __builtin_popcount(x xor y); }
+struct Solution {
+  auto hammingDistance(const int x, const int y) -> int { return __builtin_popcount(x xor y); }
+};
+
+struct Solution {
+  auto hammingDistance(const int x, const int y) -> int {
+    auto different_bits = 0;
+    auto result = x xor y;
+    for (auto i = 0; i < sizeof(int) * 8; ++i) {
+      if (result bitand 1 == 1) ++different_bits;
+      result = result >> 1;
+    }
+
+    return different_bits;
+  }
 };
