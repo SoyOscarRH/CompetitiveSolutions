e9cb9f7b5a99db903b92ed42316b9a80c040a97f
SoyOscarRH <soyoscarrh@gmail.com>
2020-07-06 03:12:35 -0500

    Update 66-PlusOne.cpp

--- Leetcode/66-PlusOne.cpp
+++ Leetcode/66-PlusOne.cpp
@@ -1,5 +1,4 @@
-class Solution {
- public:
+struct Solution {
   auto plusOne(vector<int> digits) -> vector<int> {
     const auto n = digits.size();
     for (int i = n - 1; i >= 0; --i) {
@@ -11,6 +10,29 @@ class Solution {
 
     digits[0] = 1;
     digits.push_back(0);
+    return digits;
+  }
+};
+
+struct Solution {
+  auto plusOne(vector<int> digits) -> vector<int> {
+    if (all_of(begin(digits), end(digits), [](const auto x) { return x == 9; })) {
+      fill(begin(digits), end(digits), 0);
+      digits.push_back(0);
+      digits[0] = 1;
+
+      return digits;
+    }
+
+    digits.back() += 1;
+
+    auto i = rbegin(digits);
+    while (*i == 10) {
+      *i = 0;
+      ++i;
+      *i += 1;
+    }
+
     return digits;
   }
 };
