fda88b4e1ae22570c6844a293eca724b67626232
SoyOscarRH <soyoscarrh@gmail.com>
2020-08-01 02:12:32 -0500

    Update 520-DetectCapital.cpp

--- Leetcode/520-DetectCapital.cpp
+++ Leetcode/520-DetectCapital.cpp
@@ -1,15 +1,13 @@
 class Solution {
+  static auto isCapital(const char x) -> bool { return 'A' <= x and x <= 'Z'; };
+
  public:
   auto detectCapitalUse(const string& word) -> bool {
-    const auto is_capital = [](char x) { return 'A' <= x and x <= 'Z'; };
-
-    const auto num_capitals =
-        count_if(begin(word), end(word),
-                 [=](char letter) { return is_capital(letter); });
+    const auto num_capitals = count_if(begin(word), end(word), isCapital);
 
     if (word.size() == num_capitals) return true;
     if (num_capitals == 0) return true;
-    if (num_capitals == 1 and is_capital(word[0])) return true;
+    if (num_capitals == 1 and isCapital(word[0])) return true;
 
     return false;
   }
