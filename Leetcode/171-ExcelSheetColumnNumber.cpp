2f53977fac84620e81934b322f0e3d84e90b8f18
SoyOscarRH <soyoscarrh@gmail.com>
2020-08-10 21:16:41 -0500

    Update 171-ExcelSheetColumnNumber.cpp

--- Leetcode/171-ExcelSheetColumnNumber.cpp
+++ Leetcode/171-ExcelSheetColumnNumber.cpp
@@ -1,11 +1,8 @@
-class Solution {
- public:
+struct Solution {
   auto titleToNumber(const string& word) -> int {
-    const int n = word.size();
-
     auto result = 0;
     for (const auto letter : word) {
-      const int value = letter - 'A' + 1;
+      const int value = (letter - 'A') + 1;
       result *= 26;
       result += value;
     }
