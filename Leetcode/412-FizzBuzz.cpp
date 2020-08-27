8fdcf649d74b121d120b914a1bfb8a7dc8f4001d
SoyOscarRH <soyoscarrh@gmail.com>
2020-08-26 20:40:26 -0500

    Update 412-FizzBuzz.cpp

--- Leetcode/412-FizzBuzz.cpp
+++ Leetcode/412-FizzBuzz.cpp
@@ -1,19 +1,16 @@
-class Solution {
- public:
-  auto fizzBuzz(int n) -> vector<string> {
-    auto result = vector<string> {};
+struct Solution {
+  auto fizzBuzz(const int n) -> vector<string> {
+    auto results = vector<string> {};
 
-    for (int i = 1; i <= n; ++i) {
+    for (auto i = 1; i <= n; ++i) {
       auto current = string {};
 
       if (i % 3 == 0) current += "Fizz";
       if (i % 5 == 0) current += "Buzz";
 
-      if (current == "") current = to_string(i);
-
-      result.push_back(current);
+      results.push_back(current == "" ? to_string(i) : current);
     }
 
-    return result;
+    return results;
   }
 };
