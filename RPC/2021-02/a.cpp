297ac0e44d6145f83267f944756e1da072635d24
SoyOscarRH <soyoscarrh@gmail.com>
2021-03-06 12:34:35 -0600

    Update a.cpp

---
+++ RPC/2021-02/a.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+constexpr auto endline = '\n';
+
+auto solve() -> int { return 0; }
+
+auto main() -> int {
+  ios::sync_with_stdio(false);
+
+  auto tests = 10;
+  cin >> tests;
+
+  while (tests--) { solve(); }
+
+  return 0;
+}
