bdd8b3d574a879caee20da44527ddf7432039021
SoyOscarRH <soyoscarrh@gmail.com>
2021-03-10 13:56:56 -0600

    Update LittleElephantAndRozdil.cpp

--- Codeforces/GeneralProblems/LittleElephantAndRozdil.cpp
+++ Codeforces/GeneralProblems/LittleElephantAndRozdil.cpp
@@ -11,11 +11,6 @@ auto main() -> int {
   auto cities = vector<int>(num_cities);
   for (auto& city : cities) { cin >> city; }
 
-  if (num_cities == 1) {
-    cout << 1 << endl;
-    return 0;
-  }
-
   const auto smaller = min_element(begin(cities), end(cities));
   const auto times_smaller = count(begin(cities), end(cities), *smaller);
 
