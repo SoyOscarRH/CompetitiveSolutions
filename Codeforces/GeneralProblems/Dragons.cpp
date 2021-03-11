2062870a1eac81284d461c5c9397c2f3c1324a84
SoyOscarRH <soyoscarrh@gmail.com>
2021-03-10 20:13:09 -0600

    Create Dragons.cpp

---
+++ Codeforces/GeneralProblems/Dragons.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct dragon_t {
+  int strength, bonus;
+};
+
+auto main() -> int {
+  int life, num_dragons;
+  cin >> life >> num_dragons;
+
+  auto dragons = vector<dragon_t>(num_dragons);
+  for (auto& dragon : dragons) { cin >> dragon.strength >> dragon.bonus; }
+
+  sort(begin(dragons), end(dragons), [](const dragon_t d1, const dragon_t d2) {
+    if (d1.strength != d2.strength) { return d1.strength < d2.strength; }
+    return d1.bonus > d2.bonus;
+  });
+
+  const auto will_win_all =
+      all_of(begin(dragons), end(dragons), [current_life = life](const dragon_t dragon) mutable {
+        if (current_life <= dragon.strength) { return false; }
+        current_life += dragon.bonus;
+        return true;
+      });
+
+  cout << (will_win_all ? "YES" : "NO") << endl;
+  return 0;
+}
