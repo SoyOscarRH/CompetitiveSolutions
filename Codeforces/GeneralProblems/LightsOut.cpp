80d31ccdaaf56449b8b8880099c1c23df57cee4c
Oscar Andres Rosas Hernandez <soyoscarrh@gmail.com>
2021-03-12 23:48:20 -0600

    Update LightsOut.cpp

--- Codeforces/GeneralProblems/LightsOut.cpp
+++ Codeforces/GeneralProblems/LightsOut.cpp
@@ -1,42 +1,37 @@
-#include <algorithm>
 #include <array>
 #include <iostream>
-#include <string>
+
+using namespace std;
+using matrix = array<array<int, 3>, 3>;
 
 auto main() -> int {
-  using namespace std;
+  const auto on = 1, off = 0;
+  auto lights = matrix {{
+      {on, on, on},
+      {on, on, on},
+      {on, on, on},
+  }};
 
-  array<array<int, 3>, 3> times_pressed;
-
+  auto times_pressed = matrix {};
   for (auto& line : times_pressed) {
     for (auto& element : line) { cin >> element; }
   }
 
-  array<array<int, 3>, 3> lights = {{
-      {1, 1, 1},
-      {1, 1, 1},
-      {1, 1, 1},
-  }};
+  const auto invert = [](int& light) { light = light == on ? off : on; };
+  const auto is_valid = [](int i, int j) { return (i >= 0 and i < 3) and (j >= 0 and j < 3); };
 
-  const auto invert = [](int& x) { x = x == 0 ? 1 : 0; };
-
-  const auto is_valid = [](const int i, const int j) {
-    return (i >= 0 and i < 3) and (j >= 0 and j < 3);
-  };
-
-  const auto checkAndInvert = [&](const int i, const int j) {
-    if (is_valid(i, j)) invert(lights[i][j]);
+  const auto check_and_invert = [&](int i, int j) {
+    if (is_valid(i, j)) { invert(lights[i][j]); }
   };
 
   for (auto i = 0; i < 3; ++i) {
     for (auto j = 0; j < 3; ++j) {
-      const auto num_pressed = times_pressed[i][j];
-      for (auto time = 0; time < num_pressed; ++time) {
-        checkAndInvert(i, j);
-        checkAndInvert(i + 1, j);
-        checkAndInvert(i - 1, j);
-        checkAndInvert(i, j + 1);
-        checkAndInvert(i, j - 1);
+      if (times_pressed[i][j] % 2 == 1) {
+        check_and_invert(i, j);
+        check_and_invert(i + 1, j);
+        check_and_invert(i - 1, j);
+        check_and_invert(i, j + 1);
+        check_and_invert(i, j - 1);
       }
     }
   }
